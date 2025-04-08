23c7aa8915a2cfdb3f986d2084b69b706eee590d
sajjadmusavi <sajjadmusavii33@gmail.com>
2025-04-08 13:45:32 +0330

    feat: install tailwind and vue router and some dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -25,6 +26,7 @@ export default defineConfig({
         ],
       },
     }),
+    tailwindcss()
   ],
   server: {
     host: true,
