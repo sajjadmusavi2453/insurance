730268e39da468184d67bc306489982b001dadd0
sajjadmusavi <sajjadmusavii33@gmail.com>
2025-04-08 09:32:31 +0330

    init: initial vue project and install pwa

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["faveicon.svg", "robots.txt"],
+      manifest: {
+        name: "مدیریت کارها",
+        short_name: "بیمه سامان",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    host: true,
+  },
+});
