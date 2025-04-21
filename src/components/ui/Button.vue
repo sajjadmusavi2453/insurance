6213b10691e049600b14d8dd58f43e754a240c79
sajjadmusavi <sajjadmusavii33@gmail.com>
2025-04-21 14:50:14 +0330

    style: create text input , password input , and button

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+type PropsType = {
+  block?: boolean;
+  disabled?: boolean;
+  loading?: boolean;
+  tonal?: boolean;
+  text?: boolean;
+};
+const props = defineProps<PropsType>();
+</script>
+
+<template>
+  <button
+    :class="`bg-teal-600 text-white rounded-lg px-4 py-2 focus:ring-2 focus:ring-teal-200 hover:bg-teal-700 ${
+      block ? 'w-full' : ''
+    }
+    ${
+      tonal
+        ? '!bg-teal-100 !text-teal-600 focus:!ring-teal-600  hover:!text-teal-700'
+        : ''
+    }
+    ${
+      text
+        ? '!bg-transparent !text-teal-600 focus:!ring-0 hover:!bg-teal-50'
+        : ''
+    }`"
+  >
+    <slot> </slot>
+  </button>
+</template>
---
+++ src/components/ui/Input.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { EyeIcon, EyeSlashIcon } from "@heroicons/vue/20/solid";
+import { computed, ref, type FunctionalComponent } from "vue";
+// import InputLabel from "./InputLabel.vue";
+
+type PropsType = {
+  placeholder?: string;
+  icon?: FunctionalComponent;
+  modelValue?: string;
+  inputClass?: string;
+  title?: string;
+  desc?: string;
+  type?: "password" | "text";
+};
+
+const props = defineProps<PropsType>();
+const emit = defineEmits(["update:modelValue", "onEnter"]);
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit("update:modelValue", value);
+  },
+});
+// const reactiveData = ref("");
+const haseTitle = computed(() => !!props.title);
+</script>
+
+<template>
+  <div class="relative w-full focus-within:text-teal-800 text-gray-400">
+    <input
+      type="text"
+      v-model="value"
+      :placeholder="placeholder"
+      :class="`w-full outline-none h-full rounded-lg bg-white pl-7 rtl:pr-7 transition-all py-2 ring-2 ring-gray-300 focus:ring-2 focus:ring-teal-600`"
+      @keypress.enter="$emit('onEnter')"
+    />
+    <component
+      :is="icon"
+      class="w-5 h-5 absolute left-1 rtl:right-1 top-1/2 -translate-y-1/2"
+    />
+  </div>
+</template>
