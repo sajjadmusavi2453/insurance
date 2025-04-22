14aee9688c9fc5ae86e2e59a38a519ac4db6bdc0
sajjadmusavi <sajjadmusavii33@gmail.com>
2025-04-22 16:56:30 +0330

    style: Create divider for forms actions

---
+++ src/components/ui/Loader.vue
@@ -0,0 +1,9 @@
+<script setup lang="tsx"></script>
+
+<template>
+  <div class="flex items-center justify-center relative">
+    <div
+      class="w-5 h-5 rounded-full border-4 border-t-4 border-gray-200 border-t-teal-400 animate-spin"
+    ></div>
+  </div>
+</template>
